439f72da85fe20c62223ba1f319dc7040bde89b1
gohjy <154421097+gohjy@users.noreply.github.com>
2025-06-19 00:53:18 +0900

    Move stuff around, a few stylistic changes

---
+++ scripts/elements.js
@@ -0,0 +1,43 @@
+"use strict";
+
+class CommonHeader extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <a href="./pages/home.html" class="logo-box">
+                <span class="logo-tag">
+                    &lt;<span class="logo-asterisk">*</span>&gt;
+                </span>
+                <span class="logo-text">
+                    NUSHDEV
+                </span>
+            </a>
+
+            <a href="./pages/projects.html">Projects</a>
+            
+            <a href="./pages/contact.html">Contact</a>
+        `;
+    }
+}
+
+class CommonFooter extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <!-- <nav class="footer-nav"></nav> -->
+
+            <div class="footer-credits">
+                Page content copyright &copy; NUSHDev 2025 unless otherwise stated. 
+            </div>
+        `;
+    }
+}
+
+customElements.define("ndv-header", CommonHeader);
+customElements.define("ndv-footer", CommonFooter);
